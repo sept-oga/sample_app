072edbf62948374c6d9d420352a3d573a975e197
sept-oga <178131979+sept-oga@users.noreply.github.com>
2024-09-14 10:31:37 +0000

    [Finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,13 +3,17 @@ class ListsController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
     redirect_to '/top'
   end
-  
+
+  def index
+    @lists = List.all
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
